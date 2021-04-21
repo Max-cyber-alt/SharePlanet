d60eb55848fad887c3f9321c2332ae0e3a02b2e6
Maksym <maksymshymko@gmail.com>
2021-04-21 14:26:11 +0300

    added di modules

---
+++ app/src/main/java/com/mobiproplus/sharedplanet/di/MainModule.kt
@@ -0,0 +1,24 @@
+package com.mobiproplus.sharedplanet.di
+
+import com.mobiproplus.sharedplanet.data.DataRepository
+import com.mobiproplus.sharedplanet.data.local.LocalDataSource
+import com.mobiproplus.sharedplanet.data.network.NetworkDataSource
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MainModule {
+
+    @Provides
+    @Singleton
+    fun provideRepository(
+        localDataSource: LocalDataSource,
+        networkDataSource: NetworkDataSource
+    ): DataRepository {
+        return DataRepository(localDataSource, networkDataSource)
+    }
+}
